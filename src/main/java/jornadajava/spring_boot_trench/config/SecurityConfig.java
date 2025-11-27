2d36be4c00eb141d924c8f1d4b4c0ba87ee2a88c
Afonso Braga <afonsobraga2001@gmail.com>
2025-11-27 17:23:29 -0300

    dando total permissao para o swagger nao cobrar autorização

--- src/main/java/jornadajava/spring_boot_trench/config/SecurityConfig.java
+++ src/main/java/jornadajava/spring_boot_trench/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package jornadajava.spring_boot_trench.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -16,7 +17,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    //aqui estamos montando uma lista de coisas que o spring secutiry nao precisara cobrar autenticação
+    //e estamos dando total permissao para o swagger, pois a documentação da API nao precisa de segurança
+    private static final String[] WHITE_LIST = {"/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**"};
 
+    //aqui estou fazendo usuarios que podem ter permissao para acessar os endpoints da API
+    //e tbm estou encriptando cada senha para segurança
     @Bean
     public UserDetailsService userDetailsService() {
         log.info(passwordEncoder().encode("jokinha"));
@@ -33,12 +39,21 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(user, admin);
     }
 
+    //aqui esta sendo dada a autorização para a lista que foi criada acima
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return null;
+        return http.authorizeHttpRequests
+                (auth -> auth.requestMatchers(WHITE_LIST)
+                        //lista sendo autorizada cm permitAll
+                        .permitAll()
+                        //porem para cada outra requisição, solicite autorização
+                        .anyRequest()
+                        //este httpBasic nada mais é que aquele pop up do navegador que solicita login e senha
+                        .authenticated()).httpBasic(Customizer.withDefaults()).build();
     }
 
     @Bean
+    //o passEncoder nada mais é que a criptografia da senha, ele usa o bcrypt e chamamos essa função para criptografia
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
